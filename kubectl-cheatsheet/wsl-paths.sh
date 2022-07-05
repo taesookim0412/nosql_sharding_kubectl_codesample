a23f948f21a0643ce3aecd7ceeec345b6e4e7859
taesookim0412 <taesoo.kim0412@gmail.com>
2022-07-05 01:51:28 -0700

    mount local pv on windows and run statefulset

---
+++ kubectl-cheatsheet/wsl-paths.sh
@@ -0,0 +1,10 @@
+# https://github.com/docker/for-win/issues/7023
+
+# in wsl if we go /tmp/docker-desktop-root/mnt/host
+# we see our disk drives and wsl images
+
+# can only see a possibility of creating folders in our disk drive.
+# then accessing that through windows.
+# for deployment, remove up to the disk drive.
+
+/run/desktop/mnt/host/c/mnt/sdf
